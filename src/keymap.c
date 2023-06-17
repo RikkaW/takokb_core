e1e8ec50d65f691401d879bfaf1ed4ccada0c8e6
RikkaW <rikka@shizuku.moe>
2023-06-18 01:31:27 +0800

    Fix typo

--- src/keymap.c
+++ src/keymap.c
@@ -68,7 +68,7 @@ action_t *keymap_get_action(uint8_t layer, uint8_t row, uint8_t column) {
 
 void keymap_set_action(uint8_t layer, uint8_t row, uint8_t column, const action_t *action) {
     keymap_handle_set_action(action, &takokb_get_keyboard_configuration()->keymaps[layer][row][column]);
-    takokb_configuration_changed(offsetof(takokb_configuration_t, encoders[layer][row][column]), sizeof(action_t));
+    takokb_configuration_changed(offsetof(takokb_configuration_t, keymaps[layer][row][column]), sizeof(action_t));
 }
 
 action_t *keymap_get_encoder_action(uint8_t layer, uint8_t encoder, uint8_t direction) {
